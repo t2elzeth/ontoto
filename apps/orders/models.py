999e48d035ea7fc5e931eb0d821734515262d931
Ulukman Atageldiev <ulukmanatageldiuulu@gmail.com>
2020-11-13 23:44:05 +0600

    Created Order model and Receiver model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='orders')
+    cart = models.ForeignKey(
+        'cart.Cart', on_delete=models.CASCADE, related_name='orders')
+    receiver = models.ForeignKey(
+        'Receiver', on_delete=models.CASCADE, related_name='orders', blank=True, null=True)
+
+    date_created = models.DateTimeField(auto_now=True)
+
+    # TODO: How to implement setting custom receiver or setting user itself as a receiver
+    # TODO: How to implement `order_is_delivered` functionality
+
+
+# TODO: Think on whether create `OrderProduct` model or not
+
+class Receiver(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=255)
+
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return 'Receiver: {}'.format(self.first_name)
