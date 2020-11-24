23637ca6f166f3be334c29874da1257ce0b3290d
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2020-11-25 00:05:00 +0600

    Added vuelidate, but never used. Using composition-api

--- frontend/src/components/auth/SignUp.vue
+++ frontend/src/components/auth/SignUp.vue
@@ -3,32 +3,32 @@
     <form action="" class="form" @submit.prevent="submitForm">
       <div class="form-field">
         <label for="id_email">Почта</label>
-        <input type="text" id="id_email" v-model="email">
+        <input type="text" id="id_email" v-model="formData.email">
       </div>
 
       <div class="form-field">
         <label for="id_full_name">Полное имя</label>
-        <input type="text" id="id_full_name" v-model="full_name">
+        <input type="text" id="id_full_name" v-model="formData.full_name">
       </div>
 
       <div class="form-field">
         <label for="id_phone">Номер телефона</label>
-        <input type="text" id="id_phone" v-model="phone">
+        <input type="text" id="id_phone" v-model="formData.phone">
       </div>
 
       <div class="form-field">
         <label for="id_password">Пароль</label>
-        <input type="password" id="id_password" v-model="password">
+        <input type="password" id="id_password" v-model="formData.password">
       </div>
 
       <div class="form-field">
         <label for="id_password2">Повторите пароль</label>
-        <input type="password" id="id_password2" v-model="password2">
+        <input type="password" id="id_password2" v-model="formData.password2">
       </div>
 
       <div class="form-field">
         <label for="id_description">Описание для вашего профиля</label>
-        <input type="text" id="id_description" v-model="description">
+        <input type="text" id="id_description" v-model="formData.description">
       </div>
 
       <input type="submit" value="Sign up">
@@ -47,38 +47,33 @@ const urls = {
 
 export default {
   name: 'SignUp',
-  data() {
-    return {
-      msg: 'HelloWorld!',
+  setup() {
+    const msg = 'HelloWorld!'
+    let userData = {}
+
+    const formData = {
       email: '',
       full_name: '',
       phone: '',
       password: '',
       password2: '',
       description: '',
-      userData: {}
     }
-  },
-  created() {
-    console.log('My VueJS app has been created')
-  },
-  methods: {
-    submitForm() {
+
+    function submitForm() {
       console.log('FORM HAS BEEN SUBMITTED')
 
-      axios.post(urls.signUpUrl, {
-        full_name: this.full_name,
-        email: this.email,
-        phone: this.phone,
-        description: this.description,
-        password: this.password,
-        password2: this.password2,
-      }).then((res) => {
-        this.userData = res.data
-        console.log(this.userData)
-      }).catch((err) => {
-        console.log(err.data)
+      axios.post(urls.signUpUrl, formData)
+          .then((res) => {
+            userData = res.data
+            console.log(userData)
+          }).catch((err) => {
+        console.log(err)
       })
+    }
+
+    return {
+      msg, formData, userData, submitForm
     }
   }
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,10 @@
 import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { VuelidatePlugin } from '@vuelidate/core';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VuelidatePlugin)
 app.mount("#app");
