4e36676bb6d0c0a1ed5b8b931321344af6ec9308
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2020-12-26 15:53:14 +0600

    Code refactoring in decorators

--- backend/utils/decorators.py
+++ backend/utils/decorators.py
@@ -3,9 +3,6 @@ from django.apps import apps
 
 def control_save(fn):
     def wrapper(self, save=True, *args, **kwargs):
-        if not self:
-            raise ValueError(f'Function {fn} must have at least one positional argument: `self`')
-
         if type(self) not in apps.get_models(include_auto_created=False):
             raise ValueError(f'Function {fn} must be method of django model')
 
